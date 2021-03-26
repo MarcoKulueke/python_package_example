96a1cbbaae5c6e3516c7df05cfce4a99a545b88b
Marco Kulueke <kulueke@dkrz.de>
2021-03-26 11:46:40 +0100

    edit path again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_package_example',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
